dc044365c6a4e7394a2da3837e642c4b436022d6
kai7ua <kai7@ukr.net>
2016-06-07 21:51:30 +0300

    kdjsg

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello WORLD");
-        System.out.println("Slava huy");
-    }
-}
--- src/yahuy.java
+++
@@ -1,5 +0,0 @@
-public class yahuy {
-    public void yanehuy(){
-        return;
-    }
-}
